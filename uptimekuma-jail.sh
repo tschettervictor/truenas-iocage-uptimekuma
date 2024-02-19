6f1bd79337961bf6d71aa47df35ad7f39233fa92
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-02-19 15:47:12 -0700

    remove caddy installation

--- uptimekuma-jail.sh
+++ uptimekuma-jail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Build an iocage jail under TrueNAS 13.0 using the current release of Caddy with uptimekuma
+# Build an iocage jail under TrueNAS 13.0 and install Uptime-Kuma
 # git clone https://github.com/tschettervictor/truenas-iocage-uptimekuma
 
 # Check for root privileges
@@ -21,16 +21,9 @@ DEFAULT_GW_IP=""
 INTERFACE="vnet0"
 VNET="on"
 POOL_PATH=""
-CONFIG_PATH=""
 JAIL_NAME="uptimekuma"
 HOST_NAME=""
-SELFSIGNED_CERT=0
-STANDALONE_CERT=0
-DNS_CERT=0
-NO_CERT=0
-CERT_EMAIL=""
 CONFIG_NAME="uptimekuma-config"
-CADDYSERVER="yes"
 
 # Check for uptimekuma-config and set configuration
 SCRIPT=$(readlink -f "$0")
@@ -77,33 +70,6 @@ if [ -z "${HOST_NAME}" ]; then
   exit 1
 fi
 
-# Check cert config
-if [ $STANDALONE_CERT -eq 0 ] && [ $DNS_CERT -eq 0 ] && [ $NO_CERT -eq 0 ] && [ $SELFSIGNED_CERT -eq 0 ]; then
-  echo 'Configuration error: Either STANDALONE_CERT, DNS_CERT, NO_CERT,'
-  echo 'or SELFSIGNED_CERT must be set to 1.'
-  exit 1
-fi
-if [ $STANDALONE_CERT -eq 1 ] && [ $DNS_CERT -eq 1 ] ; then
-  echo 'Configuration error: Only one of STANDALONE_CERT and DNS_CERT'
-  echo 'may be set to 1.'
-  exit 1
-fi
-if [ $DNS_CERT -eq 1 ] && [ -z "${DNS_PLUGIN}" ] ; then
-  echo "DNS_PLUGIN must be set to a supported DNS provider."
-  echo "See https://caddyserver.com/download for available plugins."
-  echo "Use only the last part of the name.  E.g., for"
-  echo "\"github.com/caddy-dns/cloudflare\", enter \"coudflare\"."
-  exit 1
-fi
-if [ $DNS_CERT -eq 1 ] && [ "${CERT_EMAIL}" = "" ] ; then
-  echo "CERT_EMAIL must be set when using Let's Encrypt certs."
-  exit 1
-fi
-if [ $STANDALONE_CERT -eq 1 ] && [ "${CERT_EMAIL}" = "" ] ; then
-  echo "CERT_EMAIL must be set when using Let's Encrypt certs."
-  exit 1
-fi
-
 # Extract IP and netmask, sanity check netmask
 IP=$(echo ${JAIL_IP} | cut -f1 -d/)
 NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
@@ -126,9 +92,7 @@ fi
 cat <<__EOF__ >/tmp/pkg.json
 {
   "pkgs": [
-  "nano",
-  "go",
-  "git",
+  "git-lite",
   "npm-node18"
   ]
 }
@@ -150,12 +114,8 @@ rm /tmp/pkg.json
 
 # Create and mount directories
 mkdir -p "${POOL_PATH}"/uptimekuma/data
-iocage exec "${JAIL_NAME}" mkdir -p /usr/local/www/
 iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc/rc.d/
 iocage exec "${JAIL_NAME}" mkdir -p /var/run/uptimekuma/
-iocage exec "${JAIL_NAME}" mkdir -p /mnt/includes/
-iocage fstab -a "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
-
 
 #####
 #
@@ -175,74 +135,6 @@ iocage exec "${JAIL_NAME}" "chown -R uptimekuma:uptimekuma /usr/local/uptime-kum
 iocage exec "${JAIL_NAME}" "chown -R uptimekuma:uptimekuma /var/run/uptimekuma"
 iocage exec "${JAIL_NAME}" sysrc uptimekuma_enable="YES"
 
-#####
-#
-# Caddy Installation
-#
-#####
-
-if [ "${CADDYSERVER}" == "yes" ];
-then
-
-# Build xcaddy, use it to build Caddy
-if ! iocage exec "${JAIL_NAME}" "go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest"
-then
-  echo "Failed to get xcaddy, terminating."
-  exit 1
-fi
-if ! iocage exec "${JAIL_NAME}" cp /root/go/bin/xcaddy /usr/local/bin/xcaddy
-then
-  echo "Failed to move xcaddy to path, terminating."
-  exit 1
-fi
-if [ ${DNS_CERT} -eq 1 ]; then
-  if ! iocage exec "${JAIL_NAME}" xcaddy build --output /usr/local/bin/caddy --with github.com/caddy-dns/"${DNS_PLUGIN}"
-  then
-    echo "Failed to build Caddy with ${DNS_PLUGIN} plugin, terminating."
-    exit 1
-  fi  
-else
-  if ! iocage exec "${JAIL_NAME}" xcaddy build --output /usr/local/bin/caddy
-  then
-    echo "Failed to build Caddy without plugin, terminating."
-    exit 1
-  fi  
-fi
-# Generate and insall self-signed cert, if necessary
-if [ $SELFSIGNED_CERT -eq 1 ]; then
-	iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc/pki/tls/private
-	iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc/pki/tls/certs
-	openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${HOST_NAME}" -keyout "${INCLUDES_PATH}"/privkey.pem -out "${INCLUDES_PATH}"/fullchain.pem
-	iocage exec "${JAIL_NAME}" cp /mnt/includes/privkey.pem /usr/local/etc/pki/tls/private/privkey.pem
-	iocage exec "${JAIL_NAME}" cp /mnt/includes/fullchain.pem /usr/local/etc/pki/tls/certs/fullchain.pem
-fi
-if [ $STANDALONE_CERT -eq 1 ] || [ $DNS_CERT -eq 1 ]; then
-  iocage exec "${JAIL_NAME}" cp -f /mnt/includes/remove-staging.sh /root/
-fi
-if [ $NO_CERT -eq 1 ]; then
-	echo "Copying Caddyfile for no SSL"
-	iocage exec "${JAIL_NAME}" cp -f /mnt/includes/Caddyfile-nossl /usr/local/www/Caddyfile
-elif [ $SELFSIGNED_CERT -eq 1 ]; then
-	echo "Copying Caddyfile for self-signed cert"
-	iocage exec "${JAIL_NAME}" cp -f /mnt/includes/Caddyfile-selfsigned /usr/local/www/Caddyfile
-elif [ $DNS_CERT -eq 1 ]; then
-	echo "Copying Caddyfile for Lets's Encrypt DNS cert"
-	iocage exec "${JAIL_NAME}" cp -f /mnt/includes/Caddyfile-dns /usr/local/www/Caddyfile
-else
-	echo "Copying Caddyfile for Let's Encrypt cert"
-	iocage exec "${JAIL_NAME}" cp -f /mnt/includes/Caddyfile-standalone /usr/local/www/Caddyfile	
-fi
-iocage exec "${JAIL_NAME}" cp -f /mnt/includes/caddy /usr/local/etc/rc.d/
-iocage exec "${JAIL_NAME}" sed -i '' "s/yourhostnamehere/${HOST_NAME}/" /usr/local/www/Caddyfile
-iocage exec "${JAIL_NAME}" sed -i '' "s/dns_plugin/${DNS_PLUGIN}/" /usr/local/www/Caddyfile
-iocage exec "${JAIL_NAME}" sed -i '' "s/api_token/${DNS_TOKEN}/" /usr/local/www/Caddyfile
-iocage exec "${JAIL_NAME}" sed -i '' "s/youremailhere/${CERT_EMAIL}/" /usr/local/www/Caddyfile
-iocage exec "${JAIL_NAME}" sysrc caddy_config="/usr/local/www/Caddyfile"
-iocage exec "${JAIL_NAME}" sysrc caddy_enable="YES"
-else
-echo "Caddyserver will not be installed."
-fi
-
 # Don't need /mnt/includes any more, so unmount it
 iocage fstab -r "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
 
@@ -252,29 +144,7 @@ iocage restart "${JAIL_NAME}"
 echo "---------------"
 echo "Installation Complete!"
 echo "---------------"
-if [ $STANDALONE_CERT -eq 1 ] || [ $DNS_CERT -eq 1 ]; then
-  echo "You have obtained your Let's Encrypt certificate using the staging server."
-  echo "This certificate will not be trusted by your browser and will cause SSL errors"
-  echo "when you connect.  Once you've verified that everything else is working"
-  echo "correctly, you should issue a trusted certificate.  To do this, run:"
-  echo "  iocage exec ${JAIL_NAME} /root/remove-staging.sh"
-  echo ""
-elif [ $SELFSIGNED_CERT -eq 1 ]; then
-  echo "You have chosen to create a self-signed TLS certificate for your installation."
-  echo "installation.  This certificate will not be trusted by your browser and"
-  echo "will cause SSL errors when you connect.  If you wish to replace this certificate"
-  echo "with one obtained elsewhere, the private key is located at:"
-  echo "/usr/local/etc/pki/tls/private/privkey.pem"
-  echo "The full chain (server + intermediate certificates together) is at:"
-  echo "/usr/local/etc/pki/tls/certs/fullchain.pem"
-  echo ""
-fi
-echo "---------------"
-if [ $NO_CERT -eq 1 ]; then
-  echo "Using your web browser, go to http://${HOST_NAME} to log in"
-else
-  echo "Using your web browser, go to https://${HOST_NAME} to log in"
-fi
+echo "Using your web browser, go to http://${HOST_NAME}:3001 or http://${IP}:3001 to log in"
 echo "---------------"
 echo "New installs will ask to create admin user on startup."
 echo "If this is a reinstall, use your old user and password."
